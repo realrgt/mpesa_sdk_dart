958e16665b9d704cd47b0d0e55295d3d4ce8b155
realrgt <ergitorubildo@gmail.com>
2020-06-05 15:42:01 +0200

    added example

--- example/main.dart
+++ example/main.dart
@@ -4,14 +4,14 @@ import 'package:mpesa_sdk_dart/mpesa_sdk_dart.dart';
 main() async {
 
   String token = MpesaConfig.getBearerToken(
-    'oz5ier0z5y8bri7yhzusdrehzdmuz6xa',
-    'MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAmptSWqV7cGUUJJhUBxsMLonux24u+FoTlrb+4Kgc6092JIszmI1QUoMohaDDXSVueXx6IXwYGsjjWY32HGXj1iQhkALXfObJ4DqXn5h6E8y5/xQYNAyd5bpN5Z8r892B6toGzZQVB7qtebH4apDjmvTi5FGZVjVYxalyyQkj4uQbbRQjgCkubSi45Xl4CGtLqZztsKssWz3mcKncgTnq3DHGYYEYiKq0xIj100LGbnvNz20Sgqmw/cH+Bua4GJsWYLEqf/h/yiMgiBbxFxsnwZl0im5vXDlwKPw+QnO2fscDhxZFAwV06bgG0oEoWm9FnjMsfvwm0rUNYFlZ+TOtCEhmhtFp+Tsx9jPCuOd5h2emGdSKD8A6jtwhNa7oQ8RtLEEqwAn44orENa1ibOkxMiiiFpmmJkwgZPOG/zMCjXIrrhDWTDUOZaPx/lEQoInJoE2i43VN/HTGCCw8dKQAwg0jsEXau5ixD0GUothqvuX3B9taoeoFAIvUPEq35YulprMM7ThdKodSHvhnwKG82dCsodRwY428kg2xM/UjiTENog4B6zzZfPhMxFlOSFX4MnrqkAS+8Jamhy1GgoHkEMrsT5+/ofjCx0HjKbT5NuA2V/lmzgJLl3jIERadLzuTYnKGWxVJcGLkWXlEPYLbiaKzbJb2sYxt+Kt5OxQqC1MCAwEAAQ==',
+    'API_KEY_HERE',
+    'PUBLIC_KEY_HERE',
   );
 
   PaymentRequest payload = PaymentRequest(
     inputTransactionReference: 'T12344C',
-    inputCustomerMsisdn: '258847522988',
-    inputAmount: 7,
+    inputCustomerMsisdn: '25884xxxxxxx',
+    inputAmount: 10,
     inputThirdPartyReference: '11114',
     inputServiceProviderCode: '171717',
   );
